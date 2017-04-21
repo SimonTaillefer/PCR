33ba6df4aa78eed7e8612fad89bbfa9731f48e6e
DieniusLy <lydieynaly@gmail.com>
2017-04-21 15:52:20 +0200

    index

---
+++ PHP/Pages/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>
+		PCR- Plateforme Collaborative de Recherche
+	</title>
+	<meta charset="utf-8">
+  	<link rel="stylesheet" type="text/css" href="..\..\Styles\style.css">
+	</head>
+
+	<div id="apercu">
+
+
+
+		<footer>
+			<CENTER> Copyright pcr.fr </CENTER>
+		</footer>
+	</div>
+
+</html>
