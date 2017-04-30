aee81ee2a2792bf04a22dcc987d58422f1bc6da4
s.taillefer <bateur82@hotmail.fr>
2017-04-30 20:15:44 +0200

    Entete + navigateur

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+	  <link href="Styles/style.css" rel="stylesheet">
+	  <?php include('PHP/Includes/head.php'); ?>
+  </head>
+
+  <body>
+    <div class="container">
+	  <?php include('PHP/Includes/header.html'); ?>
+      <!-- Static navbar -->
+      <?php include('PHP/Includes/nav.html'); ?>
+    </div> <!-- /container -->
+    <?php include('PHP/Includes/js.php'); ?>
+  </body>
+</html>
