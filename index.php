256ac221193da54f8786d6b8a38fd66d4888dc83
lrody <llv98860@gmail.com>
2017-05-12 10:26:35 +0200

    page accueil
    debut de redaction de la page d’accueil

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
       <!-- Static navbar -->
       <?php include('PHP/Includes/nav.php'); ?>
       <div id="contenu">
-        
+        <h1><center>Accueil</center></h1>
+        <p> PCR est l'outil de collaboration par excellence!!!</p>
       </div>
       <?php include('PHP/Includes/footer.php'); ?>
     </div> <!-- /container -->
