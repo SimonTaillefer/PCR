a49d296d2a454d568d40f8da0e0df2cb6f0180bc
lrody <llv98860@gmail.com>
2017-05-17 14:34:15 +0200

    page accueil
    rédaction des informations disponibles sur la page d’accueil

--- index.php
+++ index.php
@@ -13,7 +13,16 @@
       <?php include('PHP/Includes/nav.php'); ?>
       <div id="contenu">
         <h1><center>Accueil</center></h1>
-        <p> PCR est l'outil de collaboration par excellence!!!</p>
+        <p>L’environnement du financement de la recherche a changé : passage d’un mode de « financement direct » des laboratoires à un mode de « financement par projet », via des institutions et des programmations dédiées. Le management de projets n' a jamais eu autant d'importance qu'aujourd'hui.</p>
+
+        <p>PCR a été développée donc l'optique d'être un outil de collaboration qui permet à des chercheurs de pouvoir mener à bien leurs projets en apportant:</p>
+        <ul>
+          <li>une plateforme simple d'utilisation</li>
+          <li>une plateforme qui centralise les informations</li>
+          <li>une plateforme qui facilite la communication intra et inter équipes de chercheurs</li>
+          <li>une plateforme qui promeut la recherche en mettant à disposition de tous les informations issues des dernières recherches</li>
+        </ul>
+        <p> 
       </div>
       <?php include('PHP/Includes/footer.php'); ?>
     </div> <!-- /container -->
